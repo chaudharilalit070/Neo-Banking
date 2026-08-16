abf7235d79d70785c9e0bbdcea16ef5457835e21
chaudharilalit070 <chaudharilalit717@gmail.com>
2026-08-16 21:26:19 +0530

    commit common exception CustomerController  updated

--- src/main/java/com/neobank/neobank_backend/customer/api/CustomerController.java
+++ src/main/java/com/neobank/neobank_backend/customer/api/CustomerController.java
@@ -1,4 +1,40 @@
 package com.neobank.neobank_backend.customer.api;
 
+import com.neobank.neobank_backend.common.api.ApiResponse;
+import com.neobank.neobank_backend.customer.api.request.CustomerResponse;
+import com.neobank.neobank_backend.customer.application.CustomerService;
+import jakarta.validation.Valid;
+import org.slf4j.MDC;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/customers")
 public class CustomerController {
+
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<CustomerResponse>> createCustomer(
+            @Valid @RequestBody CreateCustomerRequest request
+    ) {
+        CustomerResponse response =
+                customerService.createCustomer(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(
+                        ApiResponse.success(
+                                MDC.get("correlationId"),
+                                response
+                        )
+                );
+    }
 }
