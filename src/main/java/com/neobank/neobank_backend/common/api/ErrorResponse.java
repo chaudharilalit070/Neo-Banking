7c9f9d1ec98e01d49c402042d1f68bfa55c670df
chaudharilalit070 <chaudharilalit717@gmail.com>
2026-08-16 20:51:18 +0530

    commit commmon exception ErrorResponse

--- src/main/java/com/neobank/neobank_backend/common/api/ErrorResponse.java
+++ src/main/java/com/neobank/neobank_backend/common/api/ErrorResponse.java
@@ -1,4 +1,24 @@
 package com.neobank.neobank_backend.common.api;
 
-public class ErrorResponse {
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import java.time.Instant;
+import java.util.List;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public record ErrorResponse(
+        Instant timestamp,
+        int status,
+        String errorCode,
+        String message,
+        String correlationId,
+        List<FieldError> fieldErrors
+) {
+
+    public record FieldError(
+            String field,
+            String message
+    ) {
+    }
 }
