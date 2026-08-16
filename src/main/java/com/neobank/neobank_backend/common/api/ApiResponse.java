5d782c11c60aacc9cc29fdb07df757343f225d70
chaudharilalit070 <chaudharilalit717@gmail.com>
2026-08-16 20:50:33 +0530

    commit creating packages and apiresponsse

--- src/main/java/com/neobank/neobank_backend/common/api/ApiResponse.java
+++ src/main/java/com/neobank/neobank_backend/common/api/ApiResponse.java
@@ -1,4 +1,25 @@
 package com.neobank.neobank_backend.common.api;
 
-public class ApiResponse {
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import java.time.Instant;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public record ApiResponse<T>(
+        Instant timestamp,
+        String correlationId,
+        T data
+) {
+
+    public static <T> ApiResponse<T> success(
+            String correlationId,
+            T data
+    ) {
+        return new ApiResponse<>(
+                Instant.now(),
+                correlationId,
+                data
+        );
+    }
 }
