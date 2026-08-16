e6f8772f70d94d62ca3711600e3321fa64656317
chaudharilalit070 <chaudharilalit717@gmail.com>
2026-08-16 21:03:09 +0530

    commit common exception busenessexception updated

--- src/main/java/com/neobank/neobank_backend/common/exception/BusinessException.java
+++ src/main/java/com/neobank/neobank_backend/common/exception/BusinessException.java
@@ -1,4 +1,22 @@
 package com.neobank.neobank_backend.common.exception;
 
-public class BusinessException {
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class BusinessException extends RuntimeException {
+
+    private final String errorCode;
+    private final HttpStatus status;
+
+    public BusinessException(
+            String errorCode,
+            String message,
+            HttpStatus status
+    ) {
+        super(message);
+        this.errorCode = errorCode;
+        this.status = status;
+    }
 }
